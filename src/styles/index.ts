faec9e2c2235d68bd15c49852cefc483103621c6
xh-shen <9509394@qq.com>
2025-10-07 13:33:24 +0800

    feat: update theme and pro-design-vue version

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,9 +2,10 @@
  * @Author: shen
  * @Date: 2025-05-29 10:17:31
  * @LastEditors: shen
- * @LastEditTime: 2025-09-08 16:04:31
+ * @LastEditTime: 2025-10-01 18:04:34
  * @Description:
  */
+import './css-var.less'
 import './nprogress.less'
 import './transition.less'
 import './antd.less'
